9c55d5f460253e50ec99cc777e40d56461ee65c1
dimaeev <alexeeev.dima@gmail.com>
2024-09-25 20:38:48 +0700

    Changes made

--- Lab3_1.cpp
+++ Lab3_1.cpp
@@ -19,14 +19,16 @@ int main() {
     double rt2 = sqrt(g * k / m) ;
     
     double times[] = {0.0, 0.25, 0.75, 1.0, 2.0, 3.0, 4.0, 5.0} ;
+    const int size = 8 ;
     
     cout << fixed << setprecision(3) ;
     
-    for (double t : times) {
-        double v = rt1 * tanh(rt2 * t);
-        cout << "Время: " << t << ", Скорость: " << v << endl ;
+    int i = 0 ;
+    while(i < size) {
+        double v = rt1 * tanh(rt2 * times[i]) ;
+        cout << "Время: " << times[i] << ", Скорость: " << v << endl ;
+        i++ ;
     }
     
     return 0 ;
 }
- 
